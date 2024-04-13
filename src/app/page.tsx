db635b2b016c08d94f85ee0645eae978af39d1b4
Shantanu Lad <shaan.lad@gmail.com>
2024-04-13 13:02:03 -0500

    Code Refactor & Routing Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,20 @@
 'use client'
-import axios from 'axios';
-import { url } from 'inspector';
-import { FormEvent } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation'
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
-import { any } from 'prop-types';
-import { NextPageContext } from 'next'
-import { useState, useEffect } from 'react'
 
 export default function Home() {
-
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(true)
- 
-  useEffect(() => {
-    fetch('http://localhost:3030/product/')
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-        console.log('products >> ', data)
-    })
-  }, [])
-
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No data</p>
-  
-  // export default function Home() {
-  // const router = useRouter();
-  // const products: any = Object.entries(TableData());
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm  ">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-950"> 
-          Welcome to Anypoint Energy&nbsp; 
+          Welcome to Anypoint Energy&nbsp;
         </p>
       </div>
-      <div className="z-10 max-w-5xl items-center justify-between font-sans text-sm "> 
-        <ul>
-          {data && data.map((item: any, index: number) => (
-            <li key={index} className='text-black	fixed left-0 top-0 flex w-full justify-centre border-b border-gray-300  pb-6 pt-8 backdrop-blur-2xl dark:bg-yellow-800/30 lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 shadow-lg '>
-              <span>
-                <p>
-                  Product Name - {item.name}
-                </p>
-                <p>
-                  Product Description - {item.description}
-                </p>
-                <p>
-                  Product Price - ${item.price}
-                </p>
-              </span>
-            </li> )
-          )}
-        </ul>
-      </div>
+      <div className="bg-blue-500">
+        <Link 
+            className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            href="/redirect"> View Products
+        </Link>
+      </div>  
     </main>
   );
  }
--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,24 +1,63 @@
 
+'use client'
 import Link from 'next/link';
+import { useState, useEffect } from 'react'
 
 export default function RedirectPage(){
-    return (
-    <>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm  ">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-950"> 
-                    This is Redirect Page &nbsp; 
-                </p>
-            </div>
 
-            <div className="bg-blue-500">
-                <Link 
-                    className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                    href="/"> Back To Home
-                </Link>
-            </div> 
-        </main>
-    </>
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+ 
+    useEffect(() => {
+    fetch('http://localhost:3030/product/')
+        .then((res) => res.json())
+        .then((data) => {
+        setData(data)
+        setLoading(false)
+        console.log('products >> ', data)
+    })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No data</p>
+    
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm  ">
+                    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-950"> 
+                    Welcome to Anypoint Energy&nbsp; 
+                    </p>
+                </div>
+
+                <div className="z-10 max-w-5xl items-center justify-between font-sans text-sm"> 
+                    <ul>
+                    {data && data.map((item: any, index: number) => (
+                        <li key={index} className='text-black	fixed left-0 top-0 flex w-full justify-centre border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:bg-zinc-800/30 lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 shadow-lg hover:bg-blue-600/50'>
+                        <span>
+                            <p>
+                            {item.name}
+                            </p>
+                            <p>
+                            {item.description}
+                            </p>
+                            <p>
+                            ${item.price}
+                            </p>
+                        </span>
+                        </li> 
+                        )
+                    )}
+                    </ul>
+                </div>
+                <div className="bg-blue-500">
+                    <Link 
+                        className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                        href="/"> Back To Home (Future Logout)
+                    </Link>
+                </div> 
+            </main>
+        </>
     );
 }
 
