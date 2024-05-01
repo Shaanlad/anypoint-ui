fa84df3fbe52bc99c9883c689c82691cbc2b4615
Shantanu Lad <shaan.lad@gmail.com>
2024-05-01 00:26:05 -0500

    Minor UI Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function LandingPage() {
           <span className="block text-sm font-medium text-slate-700"> 
           <p> Email </p> 
           <input 
-            type="text" 
+            type="email" 
             className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
               focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
               disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -60,7 +60,7 @@ export default function LandingPage() {
               focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
               name="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}         
+              onChange={(e) => setEmail(e.target.value)}     
           />          
           </span>          
         </div> <br/>
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -105,101 +105,101 @@ export default function Home(){
                         {/* <Modal /> */}
                         <div className='text-black fixed left-0 top-0 flex w-full justify-centre border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:bg-white lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 shadow-lg lg:rounded-xl my-6'>
                             
-                        <div className="relative flex font-mono text-left">
-                            <form onSubmit={handleSubmit}>
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> First Name </p> 
-                                <input 
-                                    type="text" 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
-                                    value={firstName}
-                                    onChange={(e) => setFirstName(e.target.value)}        
-                                />          
-                                </span>          
-                                </div> <br/>
+                            <div className="relative flex font-mono text-left">
+                                <form onSubmit={handleSubmit}>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> First Name </p> 
+                                    <input 
+                                        type="text" 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
+                                        value={firstName}
+                                        onChange={(e) => setFirstName(e.target.value)}        
+                                    />          
+                                    </span>          
+                                    </div> <br/>
 
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Last Name </p> 
-                                <input 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                                    value={lastName}
-                                    onChange={(e) => setLastName(e.target.value)}
-                                />  
-                                </span>
-                                </div> <br/>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Last Name </p> 
+                                    <input 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                        value={lastName}
+                                        onChange={(e) => setLastName(e.target.value)}
+                                    />  
+                                    </span>
+                                    </div> <br/>
 
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Email </p> 
-                                <input 
-                                    type="email" 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />  
-                                </span>
-                                </div> <br/>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Email </p> 
+                                    <input 
+                                        type="email" 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />  
+                                    </span>
+                                    </div> <br/>
 
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Password </p> 
-                                <input 
-                                    type="password" 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />  
-                                </span>
-                                </div> <br/>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Password </p> 
+                                    <input 
+                                        type="password" 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />  
+                                    </span>
+                                    </div> <br/>
 
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Is Admin? </p> 
-                                <input 
-                                    type="checkbox" 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                                    value={isAdmin}
-                                    onChange={(e) => setIsAdmin(e.target.value)}
-                                />  
-                                </span>
-                                </div> <br/>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Is Admin? </p> 
+                                    <input 
+                                        type="checkbox" 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                        value={isAdmin}
+                                        onChange={(e) => setIsAdmin(e.target.value)}
+                                    />  
+                                    </span>
+                                    </div> <br/>
 
-                                {/* <button 
-                                className="bg-red-500 hover:bg-red-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
-                                onClick={() => setshowModal(false)}
-                                >
-                                Close
-                                </button> &nbsp;&nbsp; */}
-                                <button 
-                                className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded "
-                                >
-                                Submit
-                                </button>
-                            </form>
-                        </div>
-                    </div>  
+                                    {/* <button 
+                                    className="bg-red-500 hover:bg-red-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
+                                    onClick={() => setshowModal(false)}
+                                    >
+                                    Close
+                                    </button> &nbsp;&nbsp; */}
+                                    <button 
+                                    className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded "
+                                    >
+                                    Submit
+                                    </button>
+                                </form>
+                            </div>
+                        </div>  
                     </>
                     ) : null }
                 <br/><br/>
@@ -207,7 +207,7 @@ export default function Home(){
                 <div className="z-10 items-center justify-between font-mono text-sm w-6/12"> 
                     <ul role="list" className="p-6 divide-y divide-slate-300 ">
                     {users && users.map((item: any, index: number) => (
-                        <li key={index} className="flex py-4 first:pt-0 last:pb-0 ">
+                        <li key={index} className="flex py-4 first:pt-0 last:pb-0 hover:bg-amber-300">
                             <span className="ml-3 overflow-hidden">
                                 <p className="text-sm font-medium text-red-500">
                                     Name - {item.firstname} {item.lastname}
