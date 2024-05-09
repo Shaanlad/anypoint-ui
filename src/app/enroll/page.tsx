2a50d6aebe4d077dfff47352c231bcabf9f895f6
Shantanu Lad <shaan.lad@gmail.com>
2024-05-08 22:48:20 -0500

    UI Updates to Enrollment Journey

--- src/app/enroll/page.tsx
+++ src/app/enroll/page.tsx
@@ -12,6 +12,7 @@ export default function Enroll(){
     const [phone, setPhone] = useState('');
     const [address, setAddress] = useState('');
     const [ssn, setSSN] = useState('');
+    const [zipCode, setZipCode] = useState('');
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -19,7 +20,30 @@ export default function Enroll(){
             <Header />
             <br/><br/>
             <div className="relative flex place-items-center font-mono text-center">
+                
                 <form>
+                    <div className='block'>
+                        <span className="block text-sm font-medium text-slate-700 "> 
+                        <p> Zip Code </p> 
+                        <input 
+                        type="text" 
+                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                            invalid:border-pink-500 invalid:text-pink-600
+                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                            name="zipCode"
+                            value={zipCode}
+                            onChange={(e) => setZipCode(e.target.value)}
+                            maxLength='5'
+                        />
+                        </span> <br/>
+                        <button 
+                        className="bg-blue-500 hover:bg-amber-600 text-white font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-amber-500 rounded" 
+                        >
+                         Verify Zip Code
+                        </button>
+                    </div> <br/><br/>
                     <div className='block flex'>
                         <span className="block text-sm font-medium text-slate-700"> 
                         <p> First Name </p> 
@@ -76,12 +100,12 @@ export default function Enroll(){
                         className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                            invalid:border-pink-500 invalid:text-pink-600
-                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                            text-black" 
                             name="phone"
                             value={phone}
                             onChange={(e) => setPhone(e.target.value)}
                             pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                            maxLength="10"
                         />  
                         </span>
                         &nbsp;&nbsp;&nbsp;
@@ -97,7 +121,7 @@ export default function Enroll(){
                             name="ssn"
                             value={ssn}
                             onChange={(e) => setSSN(e.target.value)}
-                            maxlength="4"
+                            maxLength="4"
                         />  
                         </span>
                     </div> <br/>
@@ -114,7 +138,7 @@ export default function Enroll(){
                             name="ssn"
                             value={ssn}
                             onChange={(e) => setSSN(e.target.value)}
-                            maxlength="4"
+                            maxLength="4"
                         />  
                         </span>
                     </div> <br/>
@@ -128,9 +152,9 @@ export default function Enroll(){
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                             invalid:border-pink-500 invalid:text-pink-600
                             focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                            name="lastName"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
+                            name="address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
                         />  
                         </span>
                     </div> <br/>
