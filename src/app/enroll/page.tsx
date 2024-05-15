ef0e2b5ac7b14a3eb5e1d39ca2a61ee39e963795
Shantanu Lad <shaan.lad@gmail.com>
2024-05-14 22:57:11 -0400

    UI Updates to Enrollment Journey

--- src/app/enroll/page.tsx
+++ src/app/enroll/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 import Header from "../_components/header";
 import Footer from "../_components/footer";
-import { useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
+import { FormEvent } from 'react';
 
 const BASE_URL = 'http://localhost:3030';
 
@@ -13,18 +14,52 @@ export default function Enroll(){
     const [address, setAddress] = useState('');
     const [ssn, setSSN] = useState('');
     const [zipCode, setZipCode] = useState('');
+    const [cusSwitchOrMove, setCusSwitchOrMove] = useState('');
+    const [cusSwitch, setCusSwitch] = useState('');
+    const [svcStartDatePicker, setSvcStartDatePicker] = useState(false);
+
+    const onSwitchOrMoveChange = (evt: string) => {
+        console.log(evt);
+        if (cusSwitchOrMove === 'moving' || cusSwitch === 'specificDate') {
+            setSvcStartDatePicker(true);
+        }
+    }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <br/>
             <Header />
+
             <br/><br/>
-            <div className="relative flex place-items-center font-mono text-center">
+            <div className="relative font-mono text-center">
+
+                <div className='block'>
+                    <span className="block text-sm font-medium text-slate-700 flex"> 
+                    <p className="mt-3 w-1/2"> Enter Zip Code </p> 
+                    <input 
+                    type="text" 
+                    className="w-1/2 mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                        invalid:border-pink-500 invalid:text-pink-600
+                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                        name="zipCode"
+                        value={zipCode}
+                        onChange={(e) => setZipCode(e.target.value)}
+                        maxLength="5"
+                    />  
+                    </span> <br/>
+                    <button
+                    className="bg-blue-500 hover:bg-amber-600 text-white font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-amber-500 rounded" 
+                    >
+                        Verify Zip Code
+                    </button>
+                </div> <br/><br/>
                 
                 <form>
                     <div className='block'>
-                        <span className="block text-sm font-medium text-slate-700 "> 
-                        <p> Zip Code </p> 
+                        <span className="block text-sm font-medium text-slate-700"> 
+                        <p> Address </p> 
                         <input 
                         type="text" 
                         className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
@@ -32,18 +67,92 @@ export default function Enroll(){
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                             invalid:border-pink-500 invalid:text-pink-600
                             focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                            name="zipCode"
-                            value={zipCode}
-                            onChange={(e) => setZipCode(e.target.value)}
-                            maxLength='5'
-                        />
-                        </span> <br/>
-                        <button 
-                        className="bg-blue-500 hover:bg-amber-600 text-white font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-amber-500 rounded" 
-                        >
-                         Verify Zip Code
-                        </button>
+                            name="address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />  
+                        </span>
                     </div> <br/><br/>
+
+                    <div className='block'>
+                        <span className="block flex text-sm font-medium text-slate-700"> 
+                        <label htmlFor="serviceType" className="w-1/2 mt-3"> Switching or Moving? </label> &nbsp;&nbsp;&nbsp;
+                            <select 
+                                name="serviceType" 
+                                id="svcType" 
+                                className="w-1/2 mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                invalid:border-pink-500 invalid:text-pink-600
+                                focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
+                                onChange={(e) => onSwitchOrMoveChange(e.target.value)}>
+                                    <option>Choose One</option>
+                                    <option>I am switching</option>
+                                    <option>I am moving</option>
+                            </select>
+                        </span>
+                    </div> <br/>
+
+
+
+                    {(cusSwitchOrMove === 'switching') ? ( 
+                        <>
+                            <div className='block'>
+                                <span className="block flex text-sm font-medium text-slate-700"> 
+                                <label htmlFor="serviceType" className="w-1/2 mt-3"> When would you like to start your service? </label> &nbsp;&nbsp;&nbsp;
+                                    <select 
+                                        name="cusSwitch" 
+                                        id="cusSwitch"
+                                        className="w-1/2 mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
+                                        onChange={(e) => setCusSwitch(e.target.value)}>
+                                            <option value="">Choose One</option>
+                                            <option value="standardSwitch">Standard Switch Date</option>
+                                            <option value="specificDate">Specific Date</option>
+                                    </select>
+                                </span>
+                            </div>
+                        </>
+                    ) : null }  
+
+                    <br/>
+
+                    {cusSwitch === 'standardSwitch' ? ( 
+                        <>
+                            <div className='block text-black fixed left-0 top-0 flex w-full justify-centre border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:bg-white lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 shadow-lg lg:rounded-xl my-6'>
+                                <span className="block text-sm font-medium text-slate-700">
+                                <p className=""> Average 5 days to complete the process </p>                                  
+                                </span>
+                            </div>
+                        </>
+                        ) : null }
+
+                    { svcStartDatePicker ? ( 
+                        <>
+                            <div className='block text-black fixed left-0 top-0 flex w-full justify-centre border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:bg-white lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 shadow-lg lg:rounded-xl my-6'>
+                                <span className="block text-sm font-medium text-slate-700 flex"> 
+                                <p className="mt-3"> Desired Service Start Date </p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input 
+                                type="date" 
+                                className="mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                    invalid:border-pink-500 invalid:text-pink-600
+                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                    name="ssn"
+                                    value={ssn}
+                                    onChange={(e) => setSSN(e.target.value)}
+                                    maxLength="4"
+                                />  
+                                </span>
+                            </div>
+                        </>
+                        ) : null }    
+                    <br/><br/>
+
                     <div className='block flex'>
                         <span className="block text-sm font-medium text-slate-700"> 
                         <p> First Name </p> 
@@ -59,7 +168,7 @@ export default function Enroll(){
                             onChange={(e) => setFirstName(e.target.value)}     
                         />          
                         </span>
-                        &nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <span className="block text-sm font-medium text-slate-700"> 
                         <p> Last Name </p>
                         <input 
@@ -76,22 +185,6 @@ export default function Enroll(){
                         </span>
                     </div> <br/>
 
-                    <div className='block'>
-                        <span className="block text-sm font-medium text-slate-700"> 
-                        <p> Address </p> 
-                        <input 
-                        type="text" 
-                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                            invalid:border-pink-500 invalid:text-pink-600
-                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                            name="lastName"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />  
-                        </span>
-                    </div> <br/>
                     <div className='block flex'>
                         <span className="block text-sm font-medium text-slate-700"> 
                         <p> Phone Number </p> 
@@ -108,11 +201,11 @@ export default function Enroll(){
                             maxLength="10"
                         />  
                         </span>
-                        &nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <span className="block text-sm font-medium text-slate-700"> 
                         <p> Last 4 digits of SSN </p> 
                         <input 
-                        type="password" 
+                        type="text" 
                         className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -124,12 +217,13 @@ export default function Enroll(){
                             maxLength="4"
                         />  
                         </span>
-                    </div> <br/>
+                    </div> <br/><br/>
+
                     <div className='block'>
                         <span className="block text-sm font-medium text-slate-700"> 
-                        <p> Last 4 digits of SSN </p> 
+                        <p> Email Address </p> 
                         <input 
-                        type="password" 
+                        type="email" 
                         className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -141,7 +235,8 @@ export default function Enroll(){
                             maxLength="4"
                         />  
                         </span>
-                    </div> <br/>
+                    </div> <br/><br/>
+
                     <div className='block'>
                         <span className="block text-sm font-medium text-slate-700"> 
                         <p> Address </p> 
@@ -167,6 +262,7 @@ export default function Enroll(){
                 </form>
             </div>
             <br/><br/>
+
             <Footer />
         </main>
     )
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -112,76 +112,76 @@ export default function Home(){
                         {/* <Modal /> */}
                         <div className='text-black fixed left-0 top-0 flex w-full justify-centre border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:bg-white lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 shadow-lg lg:rounded-xl my-6'>
                             
-                        <div className="relative flex font-mono text-left">
-                            <form onSubmit={handleSubmit}>
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Name </p> 
-                                <input 
-                                    type="text" 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
-                                    value={productName}
-                                    onChange={(e) => setProductName(e.target.value)}        
-                                />          
-                                </span>          
-                                </div> <br/>
+                            <div className="relative flex font-mono text-left">
+                                <form onSubmit={handleSubmit}>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Name </p> 
+                                    <input 
+                                        type="text" 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black"
+                                        value={productName}
+                                        onChange={(e) => setProductName(e.target.value)}        
+                                    />          
+                                    </span>          
+                                    </div> <br/>
 
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Description </p> 
-                                <textarea 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                                    value={productDescription}
-                                    onChange={(e) => setProductDescription(e.target.value)}
-                                />  
-                                </span>
-                                </div> <br/>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Description </p> 
+                                    <textarea 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                        value={productDescription}
+                                        onChange={(e) => setProductDescription(e.target.value)}
+                                    />  
+                                    </span>
+                                    </div> <br/>
 
-                                <div className='block'>
-                                <span className="block text-sm font-medium text-slate-700"> 
-                                <p> Price </p> 
-                                <input 
-                                    type="textbox" 
-                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                                    invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
-                                    value={productPrice}
-                                    onChange={(e) => setProductPrice(e.target.value)}
-                                />  
-                                </span>
-                                </div> <br/>
+                                    <div className='block'>
+                                    <span className="block text-sm font-medium text-slate-700"> 
+                                    <p> Price </p> 
+                                    <input 
+                                        type="textbox" 
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                        invalid:border-pink-500 invalid:text-pink-600
+                                        focus:invalid:border-pink-500 focus:invalid:ring-pink-500 text-black" 
+                                        value={productPrice}
+                                        onChange={(e) => setProductPrice(e.target.value)}
+                                    />  
+                                    </span>
+                                    </div> <br/>
 
-                                {/* <button 
-                                className="bg-red-500 hover:bg-red-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
-                                onClick={() => setshowModal(false)}
-                                >
-                                Close
-                                </button> &nbsp;&nbsp; */}
-                                <button 
-                                className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded "
-                                >
-                                Submit
-                                </button>
-                                &nbsp;&nbsp;
-                                <button 
-                                    className="bg-red-500 hover:bg-red-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded "
+                                    {/* <button 
+                                    className="bg-red-500 hover:bg-red-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
                                     onClick={() => setshowModal(false)}
                                     >
-                                    Cancel
-                                </button>
-                            </form>
+                                    Close
+                                    </button> &nbsp;&nbsp; */}
+                                    <button 
+                                    className="bg-blue-500 hover:bg-blue-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded "
+                                    >
+                                    Submit
+                                    </button>
+                                    &nbsp;&nbsp;
+                                    <button 
+                                        className="bg-red-500 hover:bg-red-400 text-white items-center justify-between font-bold font-mono py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded "
+                                        onClick={() => setshowModal(false)}
+                                        >
+                                        Cancel
+                                    </button>
+                                </form>
+                            </div>
                         </div>
-                    </div>
                     </>
                     ) : null }
                 <br/><br/>
